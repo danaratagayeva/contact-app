7d49009687ba6197e99f48c8dde039060d1b1bab
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-10-13 13:31:36 -0400

    used useEffect and UseState hooks

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,20 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AddContact extends Component {
+  state = {
+    name: "",
+    email: "",
+  };
+
+  add = (event) => {
+    event.preventDefault();
+    if (this.state.name === "" || this.state.email === "") {
+      alert("All the fields are mandotary!");
+      return;
+    }
+    this.props.addContactHandler(this.state);
+    this.setState({ name: "", email: "" });
+  };
+
   render() {
     return (
-      <div className='ui main'>
+      <div className="ui main">
         <h2>Add Contact</h2>
-        <form className='ui form'>
-          <div className='field'>
+        <form className="ui form" onSubmit={this.add}>
+          <div className="field">
             <label>Name</label>
-            <input type='text' name='name' placeholder='Name' />
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={this.state.name}
+              onChange={(event) => {
+                this.setState({ name: event.target.value });
+              }}
+            />
           </div>
-          <div className='field'>
+          <div className="field">
             <label>Email</label>
-            <input type='email' name='email' placeholder='Email' />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={(event) => {
+                this.setState({ email: event.target.value });
+              }}
+            />
           </div>
-          <button className='ui button blue'>Add</button>
+          <button className="ui button blue">Add</button>
         </form>
       </div>
     );
--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,66 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import ContactList from './ContactList';
-import AddContact from './AddContact';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./Header";
+import ContactList from "./ContactList";
+import AddContact from "./AddContact";
 
 function App() {
-  const contacts = [
-    {
-      id: '1',
-      name: 'Danara',
-      email: 'danara.tagayeva@gmail.com',
-    },
-    {
-      id: '2',
-      name: 'Sandibek',
-      email: 'sandibek@gmail.com',
-    },
-    {
-      id: '3',
-      name: 'Ismail',
-      email: 'ismail.ibraimov@gmail.com',
-    },
-    {
-      id: '4',
-      name: 'Ilyas',
-      email: 'ilyas.ibraimov@gmail.com',
-    },
-    {
-      id: '5',
-      name: 'Muhammad',
-      email: 'muhammad.ibraimov@gmail.com',
-    },
-  ];
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  };
+
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY)
+    );
+    if (retrieveContacts) {
+      setContacts(retrieveContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
-    <div className='ui container'>
+    <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler} />
       <ContactList contacts={contacts} />
     </div>
   );
 }
 
 export default App;
+
+// const contacts = [
+//   {
+//     id: "1",
+//     name: "Danara",
+//     email: "danara.tagayeva@gmail.com",
+//   },
+//   {
+//     id: "2",
+//     name: "Sandibek",
+//     email: "sandibek@gmail.com",
+//   },
+//   {
+//     id: "3",
+//     name: "Ismail",
+//     email: "ismail.ibraimov@gmail.com",
+//   },
+//   {
+//     id: "4",
+//     name: "Ilyas",
+//     email: "ilyas.ibraimov@gmail.com",
+//   },
+//   {
+//     id: "5",
+//     name: "Muhammad",
+//     email: "muhammad.ibraimov@gmail.com",
+//   },
+// ];
