1912a372172853fb7bbaa83c98648caa5ebea004
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-10-13 11:55:54 -0400

    created folders:components,images. created Header, AddContact, Contact list components

---
+++ src/components/AddContact.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class AddContact extends Component {
+  render() {
+    return (
+      <div className='ui main'>
+        <h2>Add Contact</h2>
+        <form className='ui form'>
+          <div className='field'>
+            <label>Name</label>
+            <input type='text' name='name' placeholder='Name' />
+          </div>
+          <div className='field'>
+            <label>Email</label>
+            <input type='email' name='email' placeholder='Email' />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+// const AddContactList = () => {
+//   return <div></div>;
+// };
+
+export default AddContact;
